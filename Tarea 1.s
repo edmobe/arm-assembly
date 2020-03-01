1e44eb64e53336a62bd896d4e8bd915ba5417075
Eduardo M <uncorreo@ejemplo.com>
2020-03-01 17:54:58 -0600

    Add initial definitions

---
+++ Tarea 1.s
@@ -0,0 +1,54 @@
+		; ===============================================================================
+		; Tecnológico de Costa Rica
+		; CE4301: Computer Architecture I
+		; First Homework: Assembly programming in ARM
+		; Teacher: Luis Chavarría Zamora
+		; Made by: Eduardo Moya Bello
+		; Date: March 6, 2020
+		; ===============================================================================
+
+
+
+		; ===============================================================================
+		; REGISTERS
+		; ===============================================================================
+		; ---------------------------------- CONSTANTS ----------------------------------
+		; Seed (R11): Eduardo Moya ("y") -> #121
+		; Counter (R12): counts how many numbers must create the LFSR -> #20.
+		; ---------------------------------- VARIABLES ----------------------------------
+		; R10: Number created by the LFSR (starts with seed value)
+		; R8: Current number's bit in position 8
+		; R6: Current number's bit in position 6
+		; R5: Current number's bit in position 5
+		; R4: Current number's bit in position 4
+		; R1: Temporal value used to store XOR results of the LFSR
+		; ===============================================================================
+
+
+		
+		; ================================================================================
+		; LFSR
+		; ================================================================================
+		; ----------------------------- CONSTANTS DEFINITION -----------------------------
+		add R11, R0, #121			;Seed definition
+		add R12, R0, #20			;Counter definition
+		; ------------------------------------- LFSR -------------------------------------
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+		
+		
+		
